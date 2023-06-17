0ae4ec1d0ff4253364f052909b181acc604110ff
Jyotsna Joshy <jyotsnajoshy2002@gmail.com>
2023-06-17 14:30:44 +0530

    updated nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
-import Button from 'react-bootstrap/Button';
+import { Route, Routes } from "react-router-dom"
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Notfound from './pages/Notfound';
+import Header from './Units/Header';
+
 export default function App() {
   return (
-    <div></div>
+    <div>
+<Header/>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/*" element={<Notfound />} />
+  </Routes>
+    </div>
   )
 }
